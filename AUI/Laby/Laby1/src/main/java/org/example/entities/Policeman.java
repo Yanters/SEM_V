2d794767ef03084c7e0339f6b7b4c8225b8ecdf4
Yanter <fajerbool@wp.pl>
2023-10-17 09:18:03 +0200

    AUI Lab1

--- AUI/Laby/Laby1/src/main/java/org/example/entities/Policeman.java
+++ AUI/Laby/Laby1/src/main/java/org/example/entities/Policeman.java
@@ -3,13 +3,15 @@ package org.example.entities;
 import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
 @Data
 @Builder
-public class Policeman implements Comparable<Policeman>{
+public class Policeman implements Comparable<Policeman>, Serializable {
     private String name;
     private Integer rank;
 
@@ -21,6 +23,11 @@ public class Policeman implements Comparable<Policeman>{
         tickets.add(ticket);
     }
 
+    @Override
+    public String toString(){
+        return "\n Name: " + name +
+                "\n Rank: " + rank;
+    }
     @Override
     public int compareTo(Policeman policeman){
         return Comparator.comparing(Policeman::getRank).thenComparing(Policeman::getName).compare(this,policeman);
--- AUI/Laby/Laby1/src/main/java/org/example/entities/Ticket.java
+++ AUI/Laby/Laby1/src/main/java/org/example/entities/Ticket.java
@@ -4,17 +4,23 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.ToString;
 
+import java.io.Serializable;
 import java.util.Comparator;
 
 @Data
 @Builder
-public class Ticket implements Comparable<Ticket>{
+public class Ticket implements Comparable<Ticket>, Serializable {
     private String reason;
     private Integer price;
 
     @ToString.Exclude
     private Policeman policeman;
 
+    @Override
+    public String toString(){
+        return "\n Reason: " + reason +
+                "\n Price:" + price;
+    }
     @Override
     public int compareTo(Ticket ticket){
         return Comparator.comparing(Ticket::getPrice).thenComparing(Ticket::getReason).compare(this,ticket);
